6775c73528f7f68602d5d6b63194f4dc12921de5
Mayan Matos <mayanmatosferro@gmail.com>
2023-05-11 14:42:21 -0300

    adicionado mais uma função

--- git-testes.py
+++ git-testes.py
@@ -11,4 +11,10 @@ num = int(input('digite um número:'))
 while no < num:
     no = no+1
     print(no) 
-    
+
+def oshi():
+    num = int(input('digite quantas vezes é pra repetir'))
+    for i in range(0,num):
+        print('todos vão gritar e adorar\n')
+    print('vou te transformar no ídolo perfeito')
+oshi() #parece que eu só preciso usar o print quando é pra mostrar alguma coisa na tela usando o return
